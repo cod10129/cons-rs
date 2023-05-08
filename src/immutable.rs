0ff6be8475b6effbaf0d7760fa0a1b1a137c2c1b
cod10129 <110200933+cod10129@users.noreply.github.com>
2023-05-08 16:51:55 +0000

    Drop for immutable::List

--- src/immutable.rs
+++ src/immutable.rs
@@ -60,6 +60,19 @@ impl<T> Default for List<T> {
     }
 }
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
+
 /// An [iterator](Iterator) that yields references
 /// to all the elements in a list.
 pub struct Iter<'a, T> {
