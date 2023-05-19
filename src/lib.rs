214127853b34b6f49a38ec63cecb47b828f51d39
cod10129 <110200933+cod10129@users.noreply.github.com>
2023-05-19 13:29:08 +0000

    ExactSize and Fused iterators

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 //! [`LinkedList`]: alloc::collections::LinkedList
 #![no_std]
 #![warn(missing_docs)]
+#![forbid(unsafe_code)]
 
 // clippy settings
 #![warn(
@@ -21,6 +22,8 @@
 
 extern crate alloc;
 use alloc::boxed::Box;
+
+use core::iter::FusedIterator;
 
 pub mod immutable;
 
@@ -116,6 +119,24 @@ impl<T> List<T> {
     pub fn is_empty(&self) -> bool {
         self.head.is_none()
     }
+
+    /// Returns the length of the `List`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use cons_rs::List;
+    ///
+    /// let mut list = List::new();
+    /// assert_eq!(list.len(), 0);
+    ///
+    /// list.push(1);
+    /// assert_eq!(list.len(), 1);
+    /// ```
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.iter().len()
+    }
     
     /// Returns an immutable reference to the value
     /// at the head of the `List`, if it exists.
@@ -158,7 +179,7 @@ impl<T> List<T> {
         self.head.as_mut().map(|node| &mut node.elem)
     }
 
-    /// Creates an [iterator that yields immutable references](Iter)
+    /// Creates an [iterator that yields shared references](Iter)
     /// to all the elements in the `List`.
     ///
     /// To get mutable references, see [`iter_mut`](List::iter_mut).
@@ -188,7 +209,7 @@ impl<T> List<T> {
     /// Creates an [iterator that yields mutable references](IterMut)
     /// to all the elements in the `List`.
     ///
-    /// To get immutable references, see [`iter`](List::iter).
+    /// To get shared references, see [`iter`](List::iter).
     ///
     /// # Examples
     ///
@@ -233,62 +254,19 @@ impl<T> Drop for List<T> {
     }
 }
 
-/// An [iterator](Iterator) that yields immutable references
-/// to all the elements in a `List`.
-///
-/// For mutable references, see [`IterMut`].
-pub struct Iter<'a, T> {
-    next: Option<&'a Node<T>>,
-}
-
-impl<'a, T> Iterator for Iter<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next.take().map(|node| {
-            self.next = node.next.as_deref();
-            &node.elem
-        })
-    }
-}
-
-/// An [iterator](Iterator) that yields mutable references
-/// to all the elements in a `List`.
-///
-/// For immutable references, see [`Iter`].
-pub struct IterMut<'a, T> {
-    next: Option<&'a mut Node<T>>,
-}
-
-impl<'a, T> Iterator for IterMut<'a, T> {
-    type Item = &'a mut T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next.take().map(|node| {
-            self.next = node.next.as_deref_mut();
-            &mut node.elem
-        })
-    }
-}
-
-/// An [iterator](Iterator) that yields all the elements in a `List` by value.
-pub struct IntoIter<T>(List<T>);
-
-impl<T> Iterator for IntoIter<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.pop()
-    }
-}
-
 impl<T> FromIterator<T> for List<T> {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
         let mut list = List::new();
-        for elem in iter {
-            list.push(elem);
-        }
+        list.extend(iter);
         list
+    }
+}
+
+impl<T> Extend<T> for List<T> {
+    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
+        for elem in iter {
+            self.push(elem);
+        }
     }
 }
 
@@ -300,6 +278,100 @@ impl<T> IntoIterator for List<T> {
         IntoIter(self)
     }
 }
+
+/// An [iterator](Iterator) that yields shared references
+/// to all the elements in a `List`.
+///
+/// For mutable references, see [`IterMut`].
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len: usize = self.next.map_or(0, |node| {
+            let mut l = 0;
+            let mut current = Some(node);
+            while let Some(node) = current {
+                current = node.next.as_deref();
+                l += 1;
+            }
+            l
+        });
+        (len, Some(len))
+    }
+}
+
+// No methods because they all have default impls
+// NOTE:
+// Once ExactSizeIterator::is_empty is stablized (github.com/rust-lang/rust/issues/35428)
+// forward List::is_empty to it.
+impl<'a, T> ExactSizeIterator for Iter<'a, T> {}
+
+impl<'a, T> FusedIterator for Iter<'a, T> {}
+
+/// An [iterator](Iterator) that yields mutable references
+/// to all the elements in a `List`.
+///
+/// For shared references, see [`Iter`].
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        Iter { next: self.next.as_deref() }.size_hint()
+    }
+}
+
+// No methods because they all have default impls
+// NOTE:
+// Once ExactSizeIterator::is_empty is stablized (github.com/rust-lang/rust/issues/35428)
+// forward List::is_empty to it.
+impl<'a, T> ExactSizeIterator for IterMut<'a, T> {}
+
+impl<'a, T> FusedIterator for IterMut<'a, T> {}
+
+/// An [iterator](Iterator) that yields all the elements in a `List` by value.
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.iter().size_hint()
+    }
+}
+
+// No methods because they all have default impls
+// NOTE:
+// Once ExactSizeIterator::is_empty is stablized (github.com/rust-lang/rust/issues/35428)
+// forward List::is_empty to it.
+impl<T> ExactSizeIterator for IntoIter<T> {}
+
+impl<T> FusedIterator for IntoIter<T> {}
 
 #[cfg(test)]
 mod tests {
@@ -359,6 +431,37 @@ mod tests {
         assert!(list.is_empty());
     }
 
+    #[test]
+    fn len() {
+        let mut list = List::new();
+        assert_eq!(list.len(), 0);
+
+        list.push(1);
+        list.push(2);
+        assert_eq!(list.len(), 2);
+
+        list.pop();
+        assert_eq!(list.len(), 1);
+    }
+
+    #[test]
+    fn size_hints() {
+        let empty = (0, Some(0));
+        let one = (1, Some(1));
+        
+        let mut list: List<i32> = List::new();
+        assert_eq!(list.iter().size_hint(), empty);
+        assert_eq!(list.iter_mut().size_hint(), empty);
+        assert_eq!(list.into_iter().size_hint(), empty);
+
+        let mut list = List::new();
+        list.push(1);
+
+        assert_eq!(list.iter().size_hint(), one);
+        assert_eq!(list.iter_mut().size_hint(), one);
+        assert_eq!(list.into_iter().size_hint(), one);
+    }
+    
     #[test]
     fn default() {
         let mut list = List::default();
